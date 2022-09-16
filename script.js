2b31e07193103a96d9e239e2d5641ed2ab2f352d
Cazolion <redhead.terror@gmail.com>
2022-09-16 20:57:02 +1000

    Add hover colouring effect

--- script.js
+++ script.js
@@ -1,36 +1,58 @@
-let x = 0;
-
-function getNumber() {
-x = prompt("How many squares side of the grid would you like? \n (Maximum 100)");
-    if (x > 100) {
-        x = 100;
-    }
-    // else if (x != Number || x <= 0) {     // reassign x again
-    //     x = prompt("Please input a number between 1 and 100");
-    // }
-
-};
-
-getNumber();
-
-function createGrid(x,x) {
-    const container = document.getElementById('grid');
-    container.id = 'main';
-    container.className = 'container';
+function createGrid(x, x) {
+    let grid = document.createElement('div');
+    game.appendChild(grid);
+    grid.id = 'grid';
 
     for (i = 0; i < x; i++) {
         let row = document.createElement('div');
         row.className = 'row';
         row.id = 'row' + i;
-    
+
         for (j = 0; j < x; j++) {
             let box = document.createElement('div');
             box.className = 'box';
             row.appendChild(box);
         }
 
-    container.appendChild(row);
+        grid.appendChild(row);
     }
+
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach(box => box.addEventListener("mouseover", () => {
+    box.classList.add('coloured')
+}));
+
 };
 
-window.onload = createGrid(x,x);
+function getNumber() {
+    x = prompt("How many squares side of the grid would you like? \n (Maximum 100)");
+
+    if (x > 100) {
+        x = 100;
+    }
+    else if (!parseInt(x) || x <= 0) {     // reassign x again
+        getNumber();
+    }
+
+};
+
+const reset = document.querySelector('#reset');
+const game = document.querySelector('.game');
+
+let x = 0;
+
+
+getNumber();
+
+window.onload = createGrid(x, x);
+
+reset.addEventListener('click', () => {
+    const removedContainer = document.getElementById('grid');
+    removedContainer.remove();
+    getNumber();
+    createGrid(x,x);
+});
+
+
+
+
